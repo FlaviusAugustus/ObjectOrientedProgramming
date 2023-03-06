983c8ac7a4a14e555386dec7df4aad2cc75d0e7a
Gracjan Grzech <g.grzech@icloud.com>
2023-03-06 18:00:39 +0100

    added copy and move constructors and assigment operators

---
+++ Lab2/Lab2/Book.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <string>
+
+class Book {
+
+	std::string author;
+	std::string title;
+
+	public:
+	
+	Book();
+
+	//l-refference constructor
+	Book(const std::string& author, const std::string& title);
+
+	//r-refference constructor
+	Book(std::string&& author, std::string&& title);
+
+	//move constructor
+	Book(Book&& other);
+
+	//copy constructor
+	Book(const Book& other);
+
+	//getters
+	std::string getAuthor();
+
+	std::string getTitle();
+
+	//l-value setters
+	void setAuthor(const std::string& author);
+
+	void setTitle(const std::string& title);
+
+	//r-value setters
+
+	void setAuthor(std::string&& author);
+
+	void setTitle(std::string&& title);
+
+	//copy operator
+	Book& operator=(const Book& right);
+
+	//move operator
+	Book&& operator=(Book&& right);
+
+	//print operator
+	friend std::ostream& operator<<(std::ostream& out, const Book& book);
+
+};
